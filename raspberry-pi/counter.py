af393f179c201de944a650fcf1ea65bb9c2bff67
Simon Prickett <simon@crudworks.org>
2022-11-22 16:47:04 +0000

    Working on the Pi now.

--- raspberry-pi/counter.py
+++ raspberry-pi/counter.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 redis_client = redis.Redis.from_url(os.getenv("REDIS_URL"))
+pubsub_client = redis_client.pubsub(ignore_subscribe_messages=True)
 
 tm = tm1637.TM1637(clk=23, dio=24)
 tm.brightness(0)
@@ -17,12 +18,11 @@ button_count = Button(16, hold_time=0.1)
 button_reset = Button(11, hold_time=2)
 
 COUNTER_KEY_NAME = "mycounter"
-REDIS_PUB_SUB_CHANNEL_NAME = f"__keyspace@0__:${COUNTER_KEY_NAME}"
+REDIS_PUB_SUB_CHANNEL_NAME = f"__keyspace@0__:{COUNTER_KEY_NAME}"
 
 def reset_count():
     print("Resetting...")
     redis_client.delete(COUNTER_KEY_NAME)
-    #tm.number(count)
 
 def incr_count():
     print("Incrementing...")
@@ -39,4 +39,16 @@ if count is None:
 print(f"Initial count value is {count}")
 tm.number(int(count))
 
-pause()
+pubsub_client.subscribe(REDIS_PUB_SUB_CHANNEL_NAME)
+
+for message in pubsub_client.listen():
+    new_count = 0
+
+    if message["data"] == "incrby":
+        new_count = int(redis_client.get(COUNTER_KEY_NAME))
+        print(f"Count is {new_count}.")
+    elif message["data"] == "del":
+        print("Count reset to 0.")
+
+    tm.number(new_count)
+        
