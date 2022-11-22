a7723ae36ae6a4557044a0f263def03a195c29be
Simon Prickett <simon@crudworks.org>
2022-11-22 16:11:30 +0000

    Added Redis counting, no pub/sub yet.

--- raspberry-pi/counter.py
+++ raspberry-pi/counter.py
@@ -16,20 +16,20 @@ tm.brightness(0)
 button_count = Button(16, hold_time=0.1)
 button_reset = Button(11, hold_time=2)
 
-count = 0
-tm.number(count)
+COUNTER_KEY_NAME = "mycounter"
+REDIS_PUB_SUB_CHANNEL_NAME = f"__keyspace@0__:${COUNTER_KEY_NAME}"
+
+#tm.number(count)
 
 def reset_count():
-    global count
     print("Resetting...")
-    count = 0
-    tm.number(count)
+    redis_client.delete(COUNTER_KEY_NAME)
+    #tm.number(count)
 
 def incr_count():
-    global count
     print("Incrementing...")
-    count += 1
-    tm.number(count)
+    redis_client.incrby(COUNTER_KEY_NAME, amount=1)
+    #tm.number(count)
 
 button_reset.when_held = reset_count
 button_count.when_held = incr_count
